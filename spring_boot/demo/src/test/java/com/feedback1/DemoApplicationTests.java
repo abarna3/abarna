22652ec0bb6b4f796f9da8f276f58099ed198831
abarna <abarna301298@gmail.com>
2020-07-08 08:24:53 +0530

    success

---
+++ spring_boot/demo/src/test/java/com/feedback1/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feedback1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
